439c38de99af9da8e72e0d0d1446439e06e918c2
Alec Doran-Twyford <alec.dorantwyford@rightmove.co.uk>
2019-05-03 14:03:33 +0100

    reconcile unit test (as Application will have the Integration test)

--- src/test/java/com/alectronic/kata/gameoflife/WorldTest.java
+++ src/test/java/com/alectronic/kata/gameoflife/WorldTest.java
@@ -1,15 +1,19 @@
 package com.alectronic.kata.gameoflife;
 
+import com.alectronic.kata.gameoflife.exception.DeadWorldException;
+import com.alectronic.kata.gameoflife.utils.GameOfLifeUtils;
 import com.alectronic.kata.gameoflife.utils.PrinterImpl;
-import com.alectronic.kata.gameoflife.utils.RandomGenerator;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class WorldTest {
 
-//    Testing step() & toString()
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Constructor
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     @Test
     public void initBaseWorld() {
         World world = new World(new boolean[1][1]);
@@ -123,6 +127,173 @@ public class WorldTest {
         assertEquals(1,state.length);
         assertEquals(1,state[0].length);
         assertFalse(state[0][0]);
+    }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Scenarios
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Test(expected = DeadWorldException.class)
+    public void shouldRemainDeadAndThrowDeadWorldException() throws DeadWorldException {
+        boolean[][] cells = new boolean[][]{
+                {false, false, false},
+                {false, false, false},
+                {false, false, false}
+        };
+
+        World world = new World(cells);
+        world.print();
+        world.step();
+        world.print();
+        boolean[][] currentWorld = world.getState();
+
+        boolean[][] expectedWorld = new boolean[][]{
+                {false, false, false},
+                {false, false, false},
+                {false, false, false}
+        };
+
+        assertArrayEquals("Expect:\n" + GameOfLifeUtils.toString(expectedWorld),expectedWorld,currentWorld);}
+
+    @Test
+    public void shouldDieOfUnderPopulation() throws DeadWorldException {
+        boolean[][] cells = new boolean[][]{
+                {false, true, false},
+                {true, false, false},
+                {false, false, false}
+        };
+
+        World world = new World(cells);
+        world.print();
+        world.step();
+        world.print();
+        boolean[][] currentWorld = world.getState();
+
+        boolean[][] expectedWorld = new boolean[][]{
+                {false, false, false},
+                {false, false, false},
+                {false, false, false}
+        };
+
+        assertArrayEquals("Expect:\n" + GameOfLifeUtils.toString(expectedWorld),expectedWorld,currentWorld);}
+
+    @Test
+    public void shouldCreateLife() throws DeadWorldException {
+        boolean[][] cells = new boolean[][]{
+                {false, true, false},
+                {true, true, false},
+                {false, false, false}
+        };
+
+        World world = new World(cells);
+        world.print();
+        world.step();
+        world.print();
+        boolean[][] currentWorld = world.getState();
+
+        boolean[][] expectedWorld = new boolean[][]{
+                {true, true, false},
+                {true, true, false},
+                {false, false, false}
+        };
+
+        assertArrayEquals("Expect:\n" + GameOfLifeUtils.toString(expectedWorld),expectedWorld,currentWorld);
+    }
+
+    @Test
+    public void shouldCreateLifeButOnlyOneOtherLivesInProcess() throws DeadWorldException {
+        boolean[][] cells = new boolean[][]{
+                {false, true, false},
+                {true, false, true},
+                {false, false, false}
+        };
+
+        World world = new World(cells);
+        world.print();
+        world.step();
+        world.print();
+        boolean[][] currentWorld = world.getState();
+
+        boolean[][] expectedWorld = new boolean[][]{
+                {false, true, false},
+                {false, true, false},
+                {false, false, false}
+        };
+
+        assertArrayEquals("Expect:\n" + GameOfLifeUtils.toString(expectedWorld),expectedWorld,currentWorld);
+    }
+
+    @Test
+    public void shouldCreateLifeButDieInProcess() throws DeadWorldException {
+        boolean[][] cells = new boolean[][]{
+                {true, false, false},
+                {false, false, true},
+                {true, false, false}
+        };
+
+        World world = new World(cells);
+        world.print();
+        world.step();
+        world.print();
+        boolean[][] currentWorld = world.getState();
+
+        boolean[][] expectedWorld = new boolean[][]{
+                {false, false, false},
+                {false, true, false},
+                {false, false, false}
+        };
+
+        assertArrayEquals("Expect:\n" + GameOfLifeUtils.toString(expectedWorld),expectedWorld,currentWorld);
+    }
+
+
+    @Test
+    public void test() throws DeadWorldException {
+        boolean[][] cells = new boolean[][]{
+                {true, true, true},
+                {true, true, true},
+                {false, false, false}
+        };
+
+        World world = new World(cells);
+        world.print();
+        world.step();
+        world.print();
+        boolean[][] currentWorld = world.getState();
+
+        boolean[][] expectedWorld = new boolean[][]{
+                {true, false, true},
+                {true, false, true},
+                {false, true, false}
+        };
+
+        assertArrayEquals("Expect:\n" + GameOfLifeUtils.toString(expectedWorld),expectedWorld,currentWorld);
+    }
+
+    @Test
+    public void shouldKillOvercrowded() throws DeadWorldException {
+        boolean[][] cells = new boolean[][]{
+                {true, true, true},
+                {true, true, true},
+                {true, true, true}
+        };
+
+        World world = new World(cells);
+        world.print();
+        world.step();
+        world.print();
+        boolean[][] currentWorld = world.getState();
+
+        boolean[][] expectedWorld = new boolean[][]{
+                {true, false, true},
+                {false, false, false},
+                {true, false, true}
+        };
+
+        assertArrayEquals("Expect:\n" + GameOfLifeUtils.toString(expectedWorld),expectedWorld,currentWorld);
     }
 
 }
